0e91f42a3253292344667acc08e5719cce03562b
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-25 16:52:01 -0500

    Added eventListeners to the number buttons so they display their values

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,42 @@
-function add(a,b) {return a+b};
+function add(a, b) { return a + b };
 
-function subs(a,b) {return a-b};
+function subs(a, b) { return a - b };
 
-function mult(a,b) {return a*b};
+function mult(a, b) { return a * b };
 
-function div(a,b) {
-    if (b === 0){
+function div(a, b) {
+    if (b === 0) {
         alert("Can't divide by zero you dummy");
         return;
-    } 
+    }
     return (a / b);
 }
 
 function operate(operator, num1, num2) {
-    switch (operator){
+    switch (operator) {
         case "+":
-            return add(num1,num2);
+            return add(num1, num2);
         case "-":
-            return subs(num1,num2);
+            return subs(num1, num2);
         case "x":
-            return mult(num1,num2);
+            return mult(num1, num2);
         case "/":
-            return div(num1,num2);                     
+            return div(num1, num2);
     }
 }
+
+
+
+const display = document.querySelector('#display');
+const numberButtons = document.querySelectorAll(".numbers");
+numberButtons.forEach((number) => {
+    let value  = number.value;
+    // console.log(value);
+    number.addEventListener('click', () => {
+        display.textContent += value;
+        console.log(display.textContent);
+    })   
+})
+
+
+

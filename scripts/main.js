3b5d1608dc17df3bf5d3769f50d2ba169df17a5d
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-27 11:24:17 -0500

    Added functionality to the calculator, though some bugs are to be fixed. The basic behaviour of the calculator has been completed.

--- scripts/main.js
+++ scripts/main.js
@@ -29,14 +29,58 @@ function operate(operator, num1, num2) {
 
 const display = document.querySelector('#display');
 const numberButtons = document.querySelectorAll(".numbers");
+let displayValue = 0;
 numberButtons.forEach((number) => {
     let value  = number.value;
     // console.log(value);
     number.addEventListener('click', () => {
+        if (clear) {
+            display.textContent = "";
+            clear = false;
+        }
         display.textContent += value;
-        console.log(display.textContent);
+        displayValue = parseFloat(display.textContent);
+        console.log(displayValue);
+        // console.log(display.textContent);
+        canOperate = true;
     })   
 })
 
+let firstNumber = true;
+let savedNumber = 0;
+let clear = false;
+let canOperate = false;
+let previousOperator;
+let operatorValue;
+
+const operators = document.querySelectorAll(".operators");
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (canOperate) {
+            operatorValue = operator.value;
+            console.log(operatorValue);
+            if (firstNumber) {
+                savedNumber = displayValue;
+                display.textContent = "";
+                firstNumber = false;
+            } else if (display.textContent != "") {
+                savedNumber = operate(previousOperator, savedNumber, displayValue);
+                display.textContent = (Math.round(savedNumber * 1000) / 1000);
+                clear = true;
+            }
+            canOperate = false;
+            previousOperator = operatorValue;
+        }
+    })
+})
+
+const equal = document.querySelector("#equal");
+equal.addEventListener('click', () => {
+    savedNumber = operate(operatorValue, savedNumber,displayValue);
+    display.textContent = (Math.round(savedNumber * 1000) / 1000);
+    displayValue = savedNumber;
+    firstNumber = true;
+    clear = true;
+})
 
 

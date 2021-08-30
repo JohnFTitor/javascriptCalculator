2cc58f2c05f7998bfc940d0aa57f8a4242b67751
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-30 15:58:32 -0500

    Added Keyboard support to the calculator It was quite interesting. I had to re-structure the functions so they were named, not anonymous so I could use them when a key were pressed.

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ function operate(operator, num1, num2) {
             return add(num1, num2);
         case "-":
             return subs(num1, num2);
-        case "x":
+        case "*":
             return mult(num1, num2);
         case "/":
             return div(num1, num2);
@@ -48,6 +48,78 @@ function clearCalculator(){
     display.textContent = "";
 }
 
+function writeDisplay(value) {
+    if (clear) {
+        display.textContent = "";
+        clear = false;
+    }
+    display.textContent += value;
+    displayValue = parseFloat(display.textContent);
+    console.log(displayValue);
+    // console.log(display.textContent);
+    canOperate = true;
+    if (firstEntry){
+        canEqual = true;
+    }
+}
+
+function makeOperation(operator){
+    if (canOperate) {
+        operatorValue = operator.value;
+        console.log(operatorValue);
+        if (firstNumber) {
+            savedNumber = displayValue;
+            display.textContent = "";
+            firstNumber = false;
+        } else if (display.textContent != "") {
+            savedNumber = operate(previousOperator, savedNumber, displayValue);
+            if (savedNumber != "error") {
+                display.textContent = (Math.round(savedNumber * 1000) / 1000);
+                clear = true;  
+            } 
+        }
+
+        if (savedNumber == "error") {
+            clearCalculator();
+        } else {
+            firstEntry= true;
+            canOperate = false;
+            previousOperator = operatorValue;
+        }
+    }
+}
+
+function enterEqual(){
+    if (canEqual){
+        savedNumber = operate(operatorValue, savedNumber,displayValue);
+        if (savedNumber == "error") {
+            clearCalculator();
+        }else {
+            display.textContent = (Math.round(savedNumber * 1000) / 1000);
+            displayValue = savedNumber;
+            firstNumber = true;
+            clear = true;
+            canEqual = false;
+            firstEntry = false;
+        }    
+    }
+}
+
+function erase(){
+    let displayArray = display.textContent.split("");
+    displayArray.pop();
+    display.textContent = displayArray.join("");
+    displayValue = parseFloat(display.textContent);
+}
+
+function displayDot(){
+    if (display.textContent.indexOf(".") == -1){
+        display.textContent += ".";
+        displayValue = parseFloat(display.textContent);
+    }
+}
+
+
 let firstNumber = true;
 let savedNumber = 0;
 let clear = false;
@@ -64,68 +136,17 @@ const numberButtons = document.querySelectorAll(".numbers");
 numberButtons.forEach((number) => {
     let value  = number.value;
     // console.log(value);
-    number.addEventListener('click', () => {
-        if (clear) {
-            display.textContent = "";
-            clear = false;
-        }
-        display.textContent += value;
-        displayValue = parseFloat(display.textContent);
-        console.log(displayValue);
-        // console.log(display.textContent);
-        canOperate = true;
-        if (firstEntry){
-            canEqual = true;
-        }
-    })   
+    number.addEventListener('click', () => writeDisplay(value));   
 })
 
 
 const operators = document.querySelectorAll(".operators");
 operators.forEach((operator) => {
-    operator.addEventListener('click', () => {
-        if (canOperate) {
-            operatorValue = operator.value;
-            console.log(operatorValue);
-            if (firstNumber) {
-                savedNumber = displayValue;
-                display.textContent = "";
-                firstNumber = false;
-            } else if (display.textContent != "") {
-                savedNumber = operate(previousOperator, savedNumber, displayValue);
-                if (savedNumber != "error") {
-                    display.textContent = (Math.round(savedNumber * 1000) / 1000);
-                    clear = true;  
-                } 
-            }
-
-            if (savedNumber == "error") {
-                clearCalculator();
-            } else {
-                firstEntry= true;
-                canOperate = false;
-                previousOperator = operatorValue;
-            }
-        }
-    })
+    operator.addEventListener('click', () => makeOperation(operator));
 })
 
 const equal = document.querySelector("#equal");
-equal.addEventListener('click', () => {
-    if (canEqual){
-        savedNumber = operate(operatorValue, savedNumber,displayValue);
-        if (savedNumber == "error") {
-            clearCalculator();
-        }else {
-            display.textContent = (Math.round(savedNumber * 1000) / 1000);
-            displayValue = savedNumber;
-            firstNumber = true;
-            clear = true;
-            canEqual = false;
-            firstEntry = false;
-        }    
-    }
-})
+equal.addEventListener('click', enterEqual);
 
 const clearButton =document.querySelector("#clear");
 clearButton.addEventListener('click', clearCalculator);
@@ -136,17 +157,30 @@ calcButtons.forEach(calcButton => {
 });
 
 const dot = document.querySelector("#dot");
-dot.addEventListener('click', () => {
-    if (display.textContent.indexOf(".") == -1){
-        display.textContent += ".";
-        displayValue = parseFloat(display.textContent);
-    }
-})
+dot.addEventListener('click', displayDot);
 
 const backspace = document.querySelector("#backspace");
-backspace.addEventListener('click', () => {
-    let displayArray = display.textContent.split("");
-    displayArray.pop();
-    display.textContent = displayArray.join("");
-    displayValue = parseFloat(display.textContent);
+backspace.addEventListener('click', erase);
+
+let operatorsList = ["/", "*", "-", "+"];
+
+document.addEventListener('keyup', (event) => {
+    keyTouch.play();
+    if (operatorsList.includes(event.key)){
+        let operator = {
+            value: event.key,
+        }
+        makeOperation(operator);
+    } else if (event.key === "Enter" ){
+        enterEqual();
+    } else if (event.key === "."){
+        displayDot();
+    } else if (event.key === "Backspace"){
+        erase();
+    }else {
+        let value = parseInt(event.key);
+        if (value >= 0 && value <= 9){
+            writeDisplay(value);
+        }
+    }
 })
